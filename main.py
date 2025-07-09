99d5622f7f8d98a233f9a3b2269125b2c397f264
Li-yachuan <2916850681@qq.com>
2025-07-09 10:49:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ import torchvision
 import matplotlib
 from tqdm import tqdm
 
-
-from data.data_loader_one_random_uncert import BSDS_Loader, BIPED_Loader, NYUD_Loader, \
+from data.data_loader_one_random_uncert import  BIPED_Loader,BIPEDv2_Loader, NYUD_Loader, \
     BSDS_Loader, PASCAL_Loader
 
 from torch.utils.data import DataLoader
@@ -32,7 +31,7 @@ import random
 import numpy
 import ssl
 import cv2
-from utils import get_model_parm_nums,cross_entropy_loss_RCF, update_args
+from utils import get_model_parm_nums, cross_entropy_loss_RCF, update_args
 
 ssl._create_default_https_context = ssl._create_unverified_context
 from torch.distributions import Normal, Independent
@@ -40,34 +39,38 @@ from torch.distributions import Normal, Independent
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
 ### default args
 parser = argparse.ArgumentParser(description='PyTorch Training')
-parser.add_argument('-b','--batch_size', default=None, type=int, metavar='BT',help='batch size')
+parser.add_argument('-b', '--batch_size', default=None, type=int, metavar='BT', help='batch size')
 parser.add_argument('--LR', default=None, type=float, help='initial learning rate')
 parser.add_argument('--weight_decay', default=None, type=float, help='default weight decay')
-parser.add_argument('--stepsize', default=None, type=int,metavar='SS', help='learning rate step size')
+parser.add_argument('--stepsize', default=None, type=int, metavar='SS', help='learning rate step size')
 parser.add_argument('--maxepoch', default=None, type=int, help='number of total epochs to run')
 parser.add_argument('--start_epoch', default=None, type=int, help='manual epoch number (useful on restarts)')
 parser.add_argument('--print_freq', '-p', default=None, type=int, help='print frequency (default: 50)')
 
-parser.add_argument('--dataset', help='root folder of dataset',default=None)
-parser.add_argument('--itersize', default=None, type=int,metavar='IS', help='iter size')
+parser.add_argument('--dataset', help='root folder of dataset', default=None)
+parser.add_argument('--itersize', default=None, type=int, metavar='IS', help='iter size')
 parser.add_argument('--kl_weight', default=None, type=float, help='weight for kl norm loss')
-parser.add_argument('--sampling', default=None, type=int,help='sampling times in test')
-parser.add_argument('--loss_lmbda', default=None, type=float,help='hype-param of loss 1.1 for BSDS 1.3 for NYUD')
+parser.add_argument('--sampling', default=None, type=int, help='sampling times in test')
+parser.add_argument('--loss_lmbda', default=None, type=float, help='hype-param of loss 1.1 for BSDS 1.3 for NYUD')
 parser.add_argument('--distribution', default=None, type=str, help='the output distribution')
-parser.add_argument('--encoder', default=None, type=str,choices=["DDN-M36","DDN-S18","VGG","CAFORMER-M36","CAFORMER-S18","RESNET50","RESNET101"])
+parser.add_argument('--encoder', default=None, type=str,
+                    choices=["DDN-M36", "DDN-S18", "VGG", "CAFORMER-M36", "CAFORMER-S18", "RESNET50", "RESNET101"])
 parser.add_argument('--model', default=None, type=str, help=' ')
 parser.add_argument('--mode', default=None, type=str)
 parser.add_argument('--note', default=None, type=str, help=' ')
 parser.add_argument("--resume", default=None)
 parser.add_argument("--noise_rate", default=None, type=float)
 
+
 ### suggested args
-parser.add_argument('--output',"-o", help='output folder', default=None)
-parser.add_argument('--gpu',"-g", default=None, type=str,help='GPU ID')
+parser.add_argument('--output', "-o", help='output folder', default=None)
+parser.add_argument('--gpu', "-g", default=None, type=str, help='GPU ID')
 ### necessary args
 parser.add_argument("--mg", action="store_true", help="Multiple Granularity, only work during test")
 parser.add_argument("--ms", action="store_true", help="Multiple Scale, works on test set, and it will test after every epoch")
-parser.add_argument("--cfg", required=True,default="config/BSDS-DDN_M36.yaml")
+parser.add_argument("--cfg", required=True, default="config/BSDS-DDN_M36.yaml")
+
+parser.add_argument("--SAM", action="store_true", help="Using SAM to help extract feature")
 
 args = parser.parse_args()
 
@@ -97,9 +100,6 @@ if random_seed > 0:
     numpy.random.seed(random_seed)
 
 
-
-
-
 def step_lr_scheduler(optimizer, epoch, init_lr=args.LR, lr_decay_epoch=3):
     """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
 
@@ -122,6 +122,9 @@ def main():
     elif args.dataset == "BIPED":
         train_dataset = BIPED_Loader(root=args.cfg["data_pth"], split="train")
         test_dataset = BIPED_Loader(root=args.cfg["data_pth"], split="test")
+    elif args.dataset == "BIPEDv2":
+        train_dataset = BIPEDv2_Loader(root=args.cfg["data_pth"], split="train")
+        test_dataset = BIPEDv2_Loader(root=args.cfg["data_pth"], split="test")
 
 
     else:
@@ -198,7 +201,12 @@ def main():
 
     for epoch in range(args.start_epoch, args.maxepoch):
 
-        train(train_loader, model, optimizer, epoch, save_dir=join(TMP_DIR, 'epoch-%d-training-record' % epoch))
+        train(train_loader,
+              model,
+              optimizer,
+              epoch,
+              save_dir=join(TMP_DIR, 'epoch-%d-training-record' % epoch),
+              args=args)
 
         test(model, test_loader, epoch=epoch, save_dir=join(TMP_DIR, 'epoch-%d-testing-record-view' % epoch))
         if "BSDS" in args.dataset and args.ms:
@@ -206,7 +214,7 @@ def main():
         log.flush()  # write log
 
 
-def train(train_loader, model, optimizer, epoch, save_dir):
+def train(train_loader, model, optimizer, epoch, save_dir, args):
     optimizer = step_lr_scheduler(optimizer, epoch, lr_decay_epoch=args.stepsize)
 
     batch_time = Averagvalue()
@@ -218,7 +226,8 @@ def train(train_loader, model, optimizer, epoch, save_dir):
     end = time.time()
     epoch_loss = []
     counter = 0
-    for i, data in enumerate(train_loader):
+
+    for index_dataloader, data in enumerate(train_loader):
         if len(data) == 4:
             (image, label, label_mean, label_std) = data
         else:
@@ -226,16 +235,19 @@ def train(train_loader, model, optimizer, epoch, save_dir):
         # measure data loading time
         data_time.update(time.time() - end)
         image, label = image.cuda(), label.cuda()
+
         mean, std = model(image)
-        outputs_dist = Independent(Normal(loc=mean, scale=std + 0.001), 1)
-        outputs = torch.sigmoid(outputs_dist.rsample())
+
         counter += 1
         ada = 1
+
+        outputs_dist = Independent(Normal(loc=mean, scale=std + 0.001), 1)
+        outputs = torch.sigmoid(outputs_dist.rsample())
+
         bce_loss, mask = cross_entropy_loss_RCF(outputs, label, ada, args.loss_lmbda)
 
         var = torch.pow(std, 2)
-        kl_reg_loss = 0.5 * torch.sum(torch.pow(mean, 2)
-                                      + var - 1.0 - torch.log(var))
+        kl_reg_loss = 0.5 * torch.sum(torch.pow(mean, 2) + var - 1.0 - torch.log(var))
         # 1e-2
         loss = bce_loss + args.kl_weight * kl_reg_loss
 
@@ -252,8 +264,8 @@ def train(train_loader, model, optimizer, epoch, save_dir):
         if not isdir(save_dir):
             os.makedirs(save_dir)
 
-        if i % (len(train_loader) // args.print_freq) == 0:
-            info = 'Epoch: [{0}/{1}][{2}/{3}] '.format(epoch, args.maxepoch, i, len(train_loader)) + \
+        if index_dataloader % (len(train_loader) // args.print_freq) == 0:
+            info = 'Epoch: [{0}/{1}][{2}/{3}] '.format(epoch, args.maxepoch, index_dataloader, len(train_loader)) + \
                    'Time {batch_time.val:.2f} (avg:{batch_time.avg:.2f}) '.format(batch_time=batch_time) + \
                    'Loss {loss.val:.2f} (avg:{loss.avg:.2f}) '.format(loss=losses) + \
                    "bce_loss:{:.2f} kl_reg_loss:{:.2f}".format(bce_loss.item(), kl_reg_loss.item())
@@ -261,19 +273,19 @@ def train(train_loader, model, optimizer, epoch, save_dir):
             _, _, H, W = outputs.shape
 
             torchvision.utils.save_image(outputs,
-                                         join(save_dir, "iter-%d.jpg" % i))
-            label[label>1]=0.5
+                                         join(save_dir, "iter-%d.jpg" % index_dataloader))
+            label[label > 1] = 0.5
             torchvision.utils.save_image(label,
-                                         join(save_dir, "iter-%d_label.jpg" % i))
+                                         join(save_dir, "iter-%d_label.jpg" % index_dataloader))
 
             torchvision.utils.save_image((mean - mean.min()) / (mean.max() - mean.min()),
                                          join(save_dir,
                                               "iter-{}_mean_{:.2f}_{:.2f}.jpg".format(
-                                                  i, mean.max().item(), mean.min().item())))
+                                                  index_dataloader, mean.max().item(), mean.min().item())))
             torchvision.utils.save_image((std - std.min()) / (std.max() - std.min()),
                                          join(save_dir,
                                               "iter-{}_std_{:.2f}_{:.2f}.jpg".format(
-                                                  i, std.max().item(), std.min().item())))
+                                                  index_dataloader, std.max().item(), std.min().item())))
         # save checkpoint
     save_checkpoint({
         'epoch': epoch,
@@ -316,8 +328,7 @@ def test(model, test_loader, epoch, save_dir, mg=False):
             result_png.save(join(png_save_dir, "%s.png" % filename))
             io.savemat(join(mat_save_dir, "%s.mat" % filename), {'result': result}, do_compression=True)
         else:
-            # for granu in [0, 1, 1.5, 2, 2.5, 3]:
-            for granu in [-3, -2.5, -2, -1.5, -1, 0, 1, 1.5, 2, 2.5, 3]:
+            for granu in [-5,-4.5,-4,-3.5,-3, -2.5, -2, -1.5, -1,-0.5, 0]:
 
                 outputs = torch.sigmoid(mean + std * granu)
                 outputs = (outputs - outputs.min()) / (outputs.max() - outputs.min())
@@ -390,7 +401,7 @@ def multiscale_test_mg(model, test_loader, epoch, save_dir):
         os.makedirs(save_dir)
     # scale = [0.6, 1, 1.6]
     scale = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75]
-    granus = [-3, -2.5, -2, -1.5, -1, 0, 1, 1.5, 2, 2.5, 3]
+    granus = [-5,-4.5,-4,-3.5,-3, -2.5, -2, -1.5, -1,-0.5, 0]
 
     for idx, (image, filename) in enumerate(tqdm(test_loader)):
         image = image[0]
@@ -398,8 +409,8 @@ def multiscale_test_mg(model, test_loader, epoch, save_dir):
         _, H, W = image.shape
 
         for granu in granus:
-            granu_dir = os.path.join(save_dir,str(granu))
-            os.makedirs(granu_dir,exist_ok=True)
+            granu_dir = os.path.join(save_dir, str(granu))
+            os.makedirs(granu_dir, exist_ok=True)
 
             multi_fuse = np.zeros((H, W), np.float32)
             for k in range(0, len(scale)):
@@ -412,12 +423,11 @@ def multiscale_test_mg(model, test_loader, epoch, save_dir):
                 # outputs = [outputs_dist.rsample() for _ in range(args.sampling)]
                 # outputs = torch.cat(outputs, dim=1).mean(dim=1, keepdim=True)
 
-                outputs = torch.sigmoid(mean+granu*std)
+                outputs = torch.sigmoid(mean + granu * std)
 
                 # from torchvision.utils import save_image
                 # save_image(outputs,"tmp.png")
                 # exit()
-
 
                 outputs = (outputs - outputs.min()) / (outputs.max() - outputs.min())
                 result = torch.squeeze(outputs.detach()).cpu().numpy()
